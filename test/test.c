7c61ff1df7912c9efdb49b2a331854d8a35df4c3
JayLon1022 <lonjay1022@gmail.com>
2025-04-22 21:26:36 +0800

    2025-4-22

--- test/test.c
+++ test/test.c
@@ -3,7 +3,6 @@
 int main()
 {
     for (int i = 0; i < 5; i++)
-        printf("Hello Grayson~%d\n", i); 
-
+        printf("Hello Grayson~%d\n", i);
     return 0;
 }
---
+++ test/test.cpp
@@ -0,0 +1,8 @@
+//
+// Created by JayLon on 2025/4/22.
+//
+#include <iostream>
+using namespace std;
+int main(){
+  cout<<"Hello World!"<<endl;
+}
